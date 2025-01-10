57273e89911d477237f721b9a57f0c799cf59283
Al Amin <alaminpgmr@gmail.com>
2025-01-10 18:31:39 +0600

    enhance Sidebar layout with logo and account section

--- src/layouts/sidebar.jsx
+++ src/layouts/sidebar.jsx
@@ -1,5 +1,15 @@
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="flex h-full flex-col justify-between text-white/80">
+      <div className="space-y-10">
+        <div>
+          <img src="/logo.svg" alt="logo" />
+        </div>
+        <div>My Accounts</div>
+      </div>
+      <div>Contact Us</div>
+    </div>
+  );
 };
 
 export default Sidebar;
