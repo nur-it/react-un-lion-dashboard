9e07604e96c8c1551474a068de89609f12ea4918
Al Amin <alaminpgmr@gmail.com>
2025-01-10 19:37:00 +0600

    add dropdown menu component and integrate it into the header

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,5 +1,13 @@
 import { useTheme } from "@/components/theme-provider";
 import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
   DownArrow,
   MoonIcon,
   NotificationIcon,
@@ -39,27 +47,39 @@ const Header = () => {
             <MoonIcon />
           </button>
         </div>
-        <div className="bg-transparent px-2 py-1">
-          <button className="relative text-[#0A0D14] dark:text-text_disable">
+        <div className="bg-transparent">
+          <button className="relative inline-flex h-8 w-8 items-center justify-center text-[#0A0D14] dark:text-text_disable">
             <NotificationIcon />
-            <div className="absolute right-0 top-0 rounded-full bg-white p-0.5">
+            <div className="absolute right-1 top-1 rounded-full bg-white p-0.5">
               <div className="h-1.5 w-1.5 rounded-full bg-error" />
             </div>
           </button>
         </div>
-        <div className="dark:bg-dark_header inline-flex h-10 cursor-pointer items-center gap-3 dark:rounded-lg dark:px-3 dark:py-1.5">
-          <img
-            className="rounded dark:w-7"
-            src="/images/user.png"
-            alt="user-icon"
-          />
-          <span className="text-xs font-semibold leading-[130%] tracking-[-0.126px] text-[#111723] dark:text-white">
-            John Doe
-          </span>
-          <span className="text-[#898D97]">
-            <DownArrow />
-          </span>
-        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="!h-10">
+            <div className="dark:bg-dark_header inline-flex cursor-pointer items-center gap-3 dark:rounded-lg dark:px-3 dark:py-1.5">
+              <img
+                className="rounded dark:w-7"
+                src="/images/user.png"
+                alt="user-icon"
+              />
+              <span className="text-xs font-semibold leading-[130%] tracking-[-0.126px] text-[#111723] dark:text-white">
+                John Doe
+              </span>
+              <span className="text-[#898D97]">
+                <DownArrow />
+              </span>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   );
