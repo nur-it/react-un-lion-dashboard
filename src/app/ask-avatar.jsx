f404c3d4ccda04760c693e4c1dbc853fe0166031
Al Amin <alaminpgmr@gmail.com>
2025-01-19 18:52:33 +0600

    add AskAvatarPage component with PageHeading

---
+++ src/app/ask-avatar.jsx
@@ -0,0 +1,11 @@
+import PageHeading from "@/components/shared/PageHeading";
+
+const AskAvatarPage = () => {
+  return (
+    <div className="space-y-6 pb-10">
+      <PageHeading page_name="Ask the Avatar" />
+    </div>
+  );
+};
+
+export default AskAvatarPage;
