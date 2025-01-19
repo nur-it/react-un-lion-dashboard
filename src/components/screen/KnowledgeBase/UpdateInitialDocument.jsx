311239589c2c1fbb2ad3ad71b1a2aac3e2811740
SymuNadeem <msymunnadeem@gmail.com>
2025-01-19 19:37:37 +0600

    UpdateInitialDocument part work

--- src/components/screen/KnowledgeBase/UpdateInitialDocument.jsx
+++ src/components/screen/KnowledgeBase/UpdateInitialDocument.jsx
@@ -1,7 +1,13 @@
 import { Button } from "@/components/ui/button";
 import { useRef } from "react";
+import { FiLoader } from "react-icons/fi";
+import { RiCloseLine } from "react-icons/ri";
+import addFrame from "../../../assets/icon/added-frame.svg";
 import cloudIcon from "../../../assets/icon/cloud-icon.svg";
+import fileUpload from "../../../assets/icon/file-loading.svg";
+import tickMark from "../../../assets/icon/green-tick.svg";
 import info from "../../../assets/icon/information-circle.svg";
+import trashIcon from "../../../assets/icon/trash.svg";
 
 const UpdateInitialDocument = () => {
   const fileInputRef = useRef(null);
@@ -26,7 +32,7 @@ const UpdateInitialDocument = () => {
             <img src={info} alt="info" className="cursor-pointer" />
           </div>
         </div>
-        <div className="col-span-6">
+        <div className="col-span-6 space-y-3">
           <div className="flex h-[88px] w-full items-center justify-between gap-5 rounded-xl border-[1px] border-dashed border-[#CDD0D5] p-6">
             <div className="flex items-center gap-5">
               <img src={cloudIcon} alt="cloudIcon" />
@@ -51,6 +57,60 @@ const UpdateInitialDocument = () => {
               className="hidden"
               onChange={handleFileChange}
             />
+          </div>
+          <div className="w-full rounded-xl border border-gray300 bg-black/[4%] py-4 pl-[14px] pr-4">
+            <div className="flex justify-between gap-3">
+              <div className="flex items-center gap-3">
+                <img src={fileUpload} alt="fileUpload" />
+                <div>
+                  <p className="text-sm font-medium text-secondary_main">
+                    new-video.mp4
+                  </p>
+                  <div className="flex items-center gap-1">
+                    <p className="text-xs text-text_secondary">
+                      60 KB of 6.5 MB
+                    </p>
+                    <div className="h-1 w-1 rounded-full bg-[#00000080]"></div>
+                    <div className="flex items-center gap-1">
+                      <FiLoader size={15} />
+                      <p className="text-xs text-secondary_main">
+                        Uploading...
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <button>
+                <RiCloseLine />
+              </button>
+            </div>
+            <div className="mt-4 h-1.5 w-full rounded-full bg-[#0000001A]">
+              <div
+                className="h-full rounded-full bg-[#665CF3] transition-all duration-300"
+                style={{ width: `${65}%` }}
+              ></div>
+            </div>
+          </div>
+          <div className="flex w-full items-center justify-between gap-3 rounded-xl border border-gray300 bg-black/[4%] py-4 pl-[14px] pr-4">
+            <div className="flex items-center gap-3">
+              <img src={addFrame} alt="addFrame" />
+              <div>
+                <p className="text-sm font-medium text-secondary_main">
+                  my-photo.jpg
+                </p>
+                <div className="flex items-center gap-1">
+                  <p className="text-xs text-text_secondary">94 KB of 94 KB</p>
+                  <div className="h-1 w-1 rounded-full bg-[#00000080]"></div>
+                  <div className="flex items-center gap-1">
+                    <img src={tickMark} alt="tickMark" />
+                    <p className="text-xs text-secondary_main">Completed</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button>
+              <img src={trashIcon} alt="trashIcon" />
+            </button>
           </div>
         </div>
       </div>
