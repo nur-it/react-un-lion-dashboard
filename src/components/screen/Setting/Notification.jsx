70c33d0418ba53a1f0cd29ed173efcad417b6393
SymuNadeem <msymunnadeem@gmail.com>
2025-01-20 19:46:28 +0600

    setting page dark

--- src/components/screen/Setting/Notification.jsx
+++ src/components/screen/Setting/Notification.jsx
@@ -11,13 +11,13 @@ const Notification = () => {
   // State to manage selected radio button
   const [selectedOption, setSelectedOption] = useState(options[0].id);
   return (
-    <div className="space-y-4 sm:space-y-6 rounded-xl border border-gray300 p-6">
-      <h5 className="text-lg font-medium text-secondary_main">
+    <div className="space-y-4 sm:space-y-6 rounded-xl border border-gray300 dark:border-[#344054] p-6">
+      <h5 className="text-lg font-medium text-secondary_main dark:text-white">
         Notification Settings
       </h5>
       <div className="space-y-4 sm:space-y-1">
         <div className="flex items-start flex-col sm:flex-row gap-4 sm:gap-6">
-          <p className="text-sm text-text_secondary">Email Notification</p>
+          <p className="text-sm text-text_secondary dark:text-gray300">Email Notification</p>
           <div className="space-y-4">
             {options.map((option) => (
               <div key={option.id} className="flex items-center gap-2">
@@ -30,16 +30,16 @@ const Notification = () => {
                       <input
                         name="color"
                         type="radio"
-                        className="peer h-6 w-6 cursor-pointer appearance-none rounded-full border border-slate-300 transition-all checked:border-[#473BF0]"
+                        className="peer h-6 w-6 cursor-pointer appearance-none rounded-full border border-slate-300 transition-all checked:border-[#473BF0] dark:checked:border-[#948DF6]"
                         id={option.id}
                         checked={selectedOption === option.id}
                         onChange={() => setSelectedOption(option.id)}
                       />
-                      <span className="absolute left-1/2 top-1/2 h-[14px] w-[14px] -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-[#473BF0] opacity-0 transition-opacity duration-200 peer-checked:opacity-100"></span>
+                      <span className="absolute left-1/2 top-1/2 h-[14px] w-[14px] -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-[#473BF0] dark:bg-[#948DF6] opacity-0 transition-opacity duration-200 peer-checked:opacity-100"></span>
                     </label>
                   </div>
                 </div>
-                <p className="font-medium text-secondary_main">
+                <p className="font-medium text-secondary_main dark:text-white">
                   {option.label}
                 </p>
               </div>
