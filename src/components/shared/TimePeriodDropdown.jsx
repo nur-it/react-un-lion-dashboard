f9d490e57d25f4f4263cb9216c32d4ffb6b91cf3
SymuNadeem <msymunnadeem@gmail.com>
2025-01-20 12:54:25 +0600

    dashboard home with dark

--- src/components/shared/TimePeriodDropdown.jsx
+++ src/components/shared/TimePeriodDropdown.jsx
@@ -9,40 +9,39 @@ const TimePeriodDropdown = ({ options = ["Weekly", "Monthly", "Yearly"] }) => {
     <div className="relative inline-block">
       {/* Dropdown Trigger */}
       <div
-        className="flex h-9 max-w-[107px] cursor-pointer items-center justify-between gap-2 rounded-lg border border-[#D0D5DD] bg-[#FFFFFF05] px-3 py-2"
+        className="flex h-9 max-w-[107px] cursor-pointer items-center justify-between gap-2 rounded-lg border border-[#D0D5DD] bg-[#FFFFFF05] px-3 py-2 dark:border-[#FFFFFF4D]"
         onClick={() => setIsOpen(!isOpen)}
       >
-        <p className="text-sm font-medium text-text_secondary">
+        <p className="text-sm font-medium text-text_secondary dark:text-white">
           {selectedOption}
         </p>
         <FiChevronDown
-          className={`text-text_secondary transition-transform ${
+          className={`text-text_secondary transition-transform dark:text-white ${
             isOpen ? "rotate-180" : ""
           }`}
         />
       </div>
 
       {isOpen && (
-  <div className="absolute right-0 z-50">
-    <div className="relative mt-1 w-full rounded-lg border border-[#D0D5DD] bg-white py-1 shadow-md">
-      {options.map((option) => (
-        <div
-          key={option}
-          className={`cursor-pointer px-4 py-2 text-sm font-medium text-text_secondary hover:bg-[#9eb7ee10] bg-transparent ${
-            selectedOption === option ? "bg-[#FFFFFF10]" : ""
-          }`}
-          onClick={() => {
-            setSelectedOption(option);
-            setIsOpen(false);
-          }}
-        >
-          {option}
+        <div className="absolute right-0 z-50">
+          <div className="relative mt-1 w-full rounded-lg border border-[#D0D5DD] bg-white py-1 shadow-md dark:bg-[#11162b] dark:border-[#FFFFFF4D]">
+            {options.map((option) => (
+              <div
+                key={option}
+                className={`cursor-pointer bg-transparent px-4 py-2 text-sm font-medium text-text_secondary dark:text-white hover:bg-[#9eb7ee10] ${
+                  selectedOption === option ? "bg-[#FFFFFF10]" : ""
+                }`}
+                onClick={() => {
+                  setSelectedOption(option);
+                  setIsOpen(false);
+                }}
+              >
+                {option}
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
-    </div>
-  </div>
-)}
-
+      )}
     </div>
   );
 };
