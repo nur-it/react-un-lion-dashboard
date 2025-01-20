ff4a6d917b56741abb32fbbd73a6fb274a30996b
SymuNadeem <msymunnadeem@gmail.com>
2025-01-20 16:19:06 +0600

    CategoryDropdown dark

--- src/components/shared/CategoryDropdown.jsx
+++ src/components/shared/CategoryDropdown.jsx
@@ -18,7 +18,7 @@ const CategoryDropdown = ({ options }) => {
     <div className="relative w-full sm:w-[260px]">
       {/* Dropdown button */}
       <div
-        className="flex h-10 items-center justify-between gap-2.5 rounded-lg border border-gray300 bg-[#0000000F] px-4 py-3 cursor-pointer text-[#00000099]"
+        className="flex h-10 items-center justify-between gap-2.5 rounded-lg border border-gray300 bg-[#0000000F] dark:bg-[#FFFFFF0D] dark:border-[#344054] px-4 py-3 cursor-pointer text-[#00000099] dark:text-[#FFFFFF99] text-sm sm:text-base"
         onClick={toggleDropdown}
       >
         {selectedCategory} {isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
@@ -26,11 +26,11 @@ const CategoryDropdown = ({ options }) => {
 
       {/* Dropdown options */}
       {isOpen && (
-        <ul className="absolute left-0 z-10 mt-1 w-full rounded-lg border border-gray300 bg-white shadow-lg">
+        <ul className="absolute left-0 z-10 mt-1 w-full rounded-lg border border-gray300 dark:border-[#FFFFFF33] dark:bg-[#FFFFFF0A] bg-white shadow-lg">
           {options.map((option, index) => (
             <li
               key={index}
-              className="px-4 py-2 text-sm text-[#00000099] cursor-pointer hover:bg-gray-100"
+              className="px-4 py-2 text-sm text-[#00000099] dark:text-white cursor-pointer hover:dark:bg-[#FFFFFF0A] hover:bg-gray-100"
               onClick={() => selectCategory(option)}
             >
               {option}
