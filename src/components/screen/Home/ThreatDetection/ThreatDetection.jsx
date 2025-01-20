d2031c516fec7fb55a6c57e15a2af01d97b5ee48
SymuNadeem <msymunnadeem@gmail.com>
2025-01-20 20:37:41 +0600

    setting and table update

--- src/components/screen/Home/ThreatDetection/ThreatDetection.jsx
+++ src/components/screen/Home/ThreatDetection/ThreatDetection.jsx
@@ -1,19 +1,21 @@
 import search from "../../../../assets/icon/search.svg";
-// import ThreatDetectionTable from "./ThreatDetectionTable";
+import ThreatDetectionTable from "./ThreatDetectionTable";
 
 const ThreatDetection = () => {
   return (
     <div className="w-full space-y-6 rounded-lg border border-gray-200 bg-white dark:border-white/[10%] dark:bg-white/[4%]">
-      <div className="flex flex-col sm:flex-row items-center justify-between gap-6 sm:gap-3 p-4 sm:p-6">
-        <div className="w-full flex items-center justify-between sm:justify-start gap-1 sm:gap-2">
-          <h5 className="text-lg sm:text-xl font-bold text-secondary_main dark:text-white whitespace-nowrap">
+      <div className="flex flex-col items-center justify-between gap-6 p-4 sm:flex-row sm:gap-3 sm:p-6">
+        <div className="flex w-full items-center justify-between gap-1 sm:justify-start sm:gap-2">
+          <h5 className="whitespace-nowrap text-lg font-bold text-secondary_main dark:text-white sm:text-xl">
             Threat Detection
           </h5>
-          <div className="flex items-center rounded-md bg-[#F238381A] px-2 sm:px-3 py-1">
-            <p className="text-[10px] sm:text-sm text-error">8 open threats to review</p>
+          <div className="flex items-center rounded-md bg-[#F238381A] px-2 py-1 sm:px-3">
+            <p className="text-[10px] text-error sm:text-sm">
+              8 open threats to review
+            </p>
           </div>
         </div>
-        <div className="flex h-10 w-full sm:max-w-[384px] items-center gap-2.5 rounded-lg border border-gray300 bg-[#0000000F] px-4 py-3">
+        <div className="flex h-10 w-full items-center gap-2.5 rounded-lg border border-gray300 bg-[#0000000F] px-4 py-3 sm:max-w-[384px]">
           <img src={search} alt="search" />
           <input
             type="text"
@@ -22,10 +24,9 @@ const ThreatDetection = () => {
           />
         </div>
       </div>
-      <div className="">
-        {/* <ThreatDetectionTable /> */}
+      <div className="pl-4 pb-4 sm:pb-6 sm:pl-6 sm:pr-6 w-full">
+        <ThreatDetectionTable />
       </div>
-      
     </div>
   );
 };
