7339f133043e0f517812cfac33d000c84370f44e
Al Amin <alaminpgmr@gmail.com>
2025-01-21 11:52:20 +0600

    Add axios for HTTP requests and implement httpService with interceptors

---
+++ src/services/httpService.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const instance = axios.create({
+  baseURL: `${import.meta.env.VITE_APP_API_BASE_URL}`,
+  timeout: 30000,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+// Add a request interceptor
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = Cookies.get("accessToken");
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+// Add a response interceptor to handle 401 errors
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      Cookies.remove("accessToken");
+      Cookies.remove("refreshToken");
+      alert("Session expired. Please log in again.");
+      window.location.href = "/sign-in";
+    }
+    return Promise.reject(error);
+  },
+);
+
+const responseBody = (response) => response.data;
+
+const httpService = {
+  get: (url, headers = {}) => instance.get(url, { headers }).then(responseBody),
+  post: (url, body, headers = {}) =>
+    instance.post(url, body, { headers }).then(responseBody),
+  put: (url, body, headers = {}) =>
+    instance.put(url, body, { headers }).then(responseBody),
+  patch: (url, body, headers = {}) =>
+    instance.patch(url, body, { headers }).then(responseBody),
+  delete: (url, headers = {}) =>
+    instance.delete(url, { headers }).then(responseBody),
+};
+
+export default httpService;
