10027b68212bebb7a030c37beb6157b4fe881ebb
Al Amin <alaminpgmr@gmail.com>
2025-01-21 12:02:17 +0600

    Add React Query setup with QueryClient and Devtools in main entry point

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "./components/theme-provider";
@@ -5,12 +7,18 @@ import { SidebarProvider } from "./contexts/sidebar.contexts";
 import RouterProvider from "./routes";
 import "./styles/global.css";
 
+// Create a client
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <SidebarProvider>
-        <RouterProvider />
-      </SidebarProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <SidebarProvider>
+          <RouterProvider />
+        </SidebarProvider>
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
