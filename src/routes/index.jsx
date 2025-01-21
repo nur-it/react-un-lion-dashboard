bab9e578191e554bebfc487280faa8740d63269c
Al Amin <alaminpgmr@gmail.com>
2025-01-21 11:24:07 +0600

    Add custom 404 error page with navigation to home

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import AskAvatarPage from "@/app/ask-avatar";
 import HomePage from "@/app/home";
 import KnowledgeBasePage from "@/app/knowledge-base";
+import ErrorPage from "@/app/not-found";
 import SettingPage from "@/app/setting";
 import SignInPage from "@/app/sign-in";
 import RootLayout from "@/layouts/root-layout";
@@ -18,6 +19,7 @@ const RouterProvider = () => {
           <Route path="settings" element={<SettingPage />} />
         </Route>
         <Route path="sign-in" element={<SignInPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
   );
