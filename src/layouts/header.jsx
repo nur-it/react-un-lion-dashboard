4eb9ec0601affd7ca4c4dca166e7fba847d2c019
Al Amin <alaminpgmr@gmail.com>
2025-01-21 21:20:09 +0600

    Remove ThreatDetection component from HomePage and add MenuIcon to Header with responsive layout adjustments

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -8,6 +8,7 @@ import {
 import {
   DownArrow,
   LogoutIcon,
+  MenuIcon,
   MoonIcon,
   NotificationIcon,
   SettingIcon,
@@ -16,7 +17,7 @@ import {
 } from "@/components/ui/svgs";
 import { useSidebar } from "@/contexts/sidebar.contexts";
 import { cn } from "@/lib/utils";
-import { Menu, X } from "lucide-react";
+import { X } from "lucide-react";
 import { Link } from "react-router";
 
 const dropdownMenuItems = [
@@ -43,20 +44,62 @@ const Header = () => {
   return (
     <nav className="flex items-center justify-between gap-4">
       <div>
-        <button
-          onClick={toggleSidebar}
-          className={cn(
-            "rounded border-[0.5px] border-[#D0D5DD] p-1.5 lg:hidden",
-          )}
-        >
-          {!isSidebarOpen ? <Menu /> : <X />}
-        </button>
+        <Link to="/" className="hidden dark:inline-block lg:hidden">
+          <img src="/logo.svg" alt="logo" />
+        </Link>
+        <Link to="/" className="inline-block dark:hidden lg:hidden">
+          <img src="/sm-wh-logo.svg" alt="logo" />
+        </Link>
+
         <h5 className="hidden items-center gap-1 text-secondary_main dark:text-white lg:inline-flex">
           <span className="text-base leading-[0.2px]">Good Morning -</span>
           <span className="text-xl font-bold leading-[130%]">John Doe</span>
         </h5>
       </div>
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-3 lg:hidden">
+        <div className="inline-flex items-center rounded border-[0.5px] border-[#D0D5DD] p-1 dark:border-[#282C3F]">
+          <button
+            onClick={() => setTheme("light")}
+            className={cn(
+              "rounded border-[0.5px] border-white_opacity05 bg-transparent px-2 py-1 text-[#667085] dark:text-[#D0D5DD]",
+              theme === "light" &&
+                "bg-primary_main text-white shadow-secondary",
+            )}
+          >
+            <SunIcon />
+          </button>{" "}
+          <button
+            onClick={() => setTheme("dark")}
+            className={cn(
+              "rounded border-[0.5px] border-white_opacity05 bg-transparent px-2 py-1 text-[#667085] dark:text-[#D0D5DD]",
+              theme === "dark" && "bg-primary_main text-white shadow-secondary",
+            )}
+          >
+            <MoonIcon />
+          </button>
+        </div>
+        <div className="bg-transparent">
+          <button className="relative inline-flex h-8 w-8 items-center justify-center text-[#0A0D14] dark:text-text_disable">
+            <NotificationIcon />
+            <div className="absolute right-1 top-1 rounded-full bg-white p-0.5">
+              <div className="h-1.5 w-1.5 rounded-full bg-error" />
+            </div>
+          </button>
+        </div>
+        <button
+          onClick={toggleSidebar}
+          className={cn(
+            "inline-flex h-6 w-6 items-center justify-center rounded lg:hidden",
+          )}
+        >
+          {!isSidebarOpen ? (
+            <MenuIcon className="text-secondary_main dark:text-white" />
+          ) : (
+            <X />
+          )}
+        </button>
+      </div>
+      <div className="hidden items-center gap-6 lg:flex">
         <div className="inline-flex items-center rounded border-[0.5px] border-[#D0D5DD] p-1 dark:border-[#282C3F]">
           <button
             onClick={() => setTheme("light")}
