4b025366311b48718df27cf8410364465346d7e0
Al Amin <alaminpgmr@gmail.com>
2025-01-21 20:22:30 +0600

    Remove ReactQueryDevtools from main.jsx for cleaner production build

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ToastContainer } from "react-toastify";
@@ -24,7 +23,7 @@ createRoot(document.getElementById("root")).render(
           />
         </SidebarProvider>
       </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </StrictMode>,
 );
