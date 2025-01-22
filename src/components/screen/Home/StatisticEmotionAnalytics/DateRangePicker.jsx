9a8d4de6546414242f1cb82c5597e0eab07e0527
SymuNadeem <msymunnadeem@gmail.com>
2025-01-22 10:10:20 +0600

    calendar dark added

--- src/components/screen/Home/StatisticEmotionAnalytics/DateRangePicker.jsx
+++ src/components/screen/Home/StatisticEmotionAnalytics/DateRangePicker.jsx
@@ -47,7 +47,7 @@ const DateRangePicker = () => {
 
       {/* Calendar shown when clicked */}
       {isCalendarOpen && (
-        <div className="absolute right-[-16px] sm:right-0 z-10 mt-2 bg-white border border-[#DADDE5] shadow-lg  rounded-lg">
+        <div className="absolute right-[-16px] sm:right-0 z-10 mt-2 bg-white dark:bg-black dark:text-white border border-[#DADDE5] dark:border-black shadow-lg  rounded-lg">
           <DatePicker
             selected={tempStartDate || startDate}
             onChange={handleSelectDates}
@@ -60,9 +60,9 @@ const DateRangePicker = () => {
           />
 
           {/* Buttons for apply and cancel */}
-          <div className="p-2 sm:p-4 flex justify-between gap-4 border-t border-[#E2E4E9] ">
+          <div className="p-2 sm:p-4 flex justify-between gap-4 border-t border-[#E2E4E9] dark:border-[#FFFFFF33] ">
             <button
-              className="w-[50%] px-6 py-1.5 bg-white border border-[#E2E4E9] rounded-lg text-sm text-[#525866]"
+              className="w-[50%] px-6 py-1.5 bg-white border border-[#E2E4E9] rounded-lg text-sm dark:bg-black dark:text-white text-[#525866]"
               onClick={handleCancel}
             >
               Cancel
