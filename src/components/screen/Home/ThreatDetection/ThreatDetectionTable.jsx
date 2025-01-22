14c0b3350a764dd9e0db5199227a418be93ba952
SymuNadeem <msymunnadeem@gmail.com>
2025-01-22 10:42:17 +0600

    dark color added in table paginations

--- src/components/screen/Home/ThreatDetection/ThreatDetectionTable.jsx
+++ src/components/screen/Home/ThreatDetection/ThreatDetectionTable.jsx
@@ -6,9 +6,9 @@ import sortIcon from "../../../../assets/icon/sort.svg";
 const ThreatDetectionTable = () => {
   return (
     <div className="w-full">
-      <div className="relative w-full overflow-x-auto border border-[#0000001A] rounded-lg dark:border-[#FFFFFF1A]">
-        <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right bg-white dark:bg-[#FFFFFF1A]">
-          <thead className="border-b border-[#0000001A] bg-[#4444440D] dark:border-[#FFFFFF1A] text-sm text-text_secondary dark:bg-[#212639] dark:text-[#E4E7EC]">
+      <div className="relative w-full overflow-x-auto rounded-lg border border-[#0000001A] dark:border-[#FFFFFF1A]">
+        <table className="w-full bg-white text-left text-sm text-gray-500 dark:bg-[#FFFFFF1A] dark:text-gray-400 rtl:text-right">
+          <thead className="border-b border-[#0000001A] bg-[#4444440D] text-sm text-text_secondary dark:border-[#FFFFFF1A] dark:bg-[#212639] dark:text-[#E4E7EC]">
             <tr>
               <th scope="col" className="p-4">
                 <div className="flex items-center justify-between">
@@ -72,10 +72,14 @@ const ThreatDetectionTable = () => {
             {rowData.map((row, index) => (
               <tr
                 key={index}
-                className="border-b bg-white text-sm text-[#1880F0] dark:text-white dark:border-[#FFFFFF1A] dark:bg-[#161b2f]"
+                className="border-b bg-white text-sm text-[#1880F0] dark:border-[#FFFFFF1A] dark:bg-[#161b2f] dark:text-white"
               >
-                <td className="p-4 underline dark:no-underline">{row.threatType}</td>
-                <td className="p-4 underline dark:no-underline">{row.platform}</td>
+                <td className="p-4 underline dark:no-underline">
+                  {row.threatType}
+                </td>
+                <td className="p-4 underline dark:no-underline">
+                  {row.platform}
+                </td>
                 <td className="w-[25%] p-4 underline dark:no-underline">
                   <p className="line-clamp-1">{row.contentSummary}</p>
                 </td>
@@ -93,7 +97,7 @@ const ThreatDetectionTable = () => {
                 </td>
                 <td className="w-[20%] p-4">
                   <div className="flex items-center justify-between gap-1">
-                    <button className="relative flex h-8 min-w-[110px] items-center justify-between rounded-md bg-error dark:bg-opacity-50 px-3 font-medium text-white">
+                    <button className="relative flex h-8 min-w-[110px] items-center justify-between rounded-md bg-error px-3 font-medium text-white dark:bg-opacity-50">
                       Mitigate
                       <IoChevronDown />
                       {row.status && (
@@ -112,7 +116,7 @@ const ThreatDetectionTable = () => {
                 <td className="p-4">
                   {row.status && (
                     <button
-                      className={`flex h-8 w-[100px] items-center justify-center rounded-md px-3 font-medium cursor-pointer ${
+                      className={`flex h-8 w-[100px] cursor-pointer items-center justify-center rounded-md px-3 font-medium ${
                         row.status === "Mitigated"
                           ? "bg-[#473BF01A] text-primary_main"
                           : ""
@@ -143,26 +147,29 @@ const ThreatDetectionTable = () => {
           <button className="flex h-8 w-8 items-center justify-center">
             <FiChevronLeft size={22} className="text-[#667085]" />
           </button>
-          <button className="flex h-8 w-8 items-center justify-center rounded-full bg-[#0000000F] text-sm font-medium text-secondary_main">
+          <button className="flex h-8 w-8 items-center justify-center rounded-full bg-[#0000000F] text-sm font-medium text-secondary_main dark:bg-[#FFFFFF0F] dark:text-white">
             1
           </button>
-          <button className="flex h-8 w-8 items-center justify-center text-sm font-medium text-text_secondary">
+          <button className="flex h-8 w-8 items-center justify-center text-sm font-medium text-text_secondary dark:text-white">
             2
           </button>
-          <button className="flex h-8 w-8 items-center justify-center text-sm font-medium text-text_secondary">
+          <button className="flex h-8 w-8 items-center justify-center text-sm font-medium text-text_secondary dark:text-white">
             3
           </button>
-          <button className="flex h-8 w-8 items-center justify-center text-sm font-medium text-text_secondary">
+          <button className="flex h-8 w-8 items-center justify-center text-sm font-medium text-text_secondary dark:text-white">
             4
           </button>
-          <button className="flex h-8 w-8 items-center justify-center text-sm font-medium text-text_secondary">
+          <button className="flex h-8 w-8 items-center justify-center text-sm font-medium text-text_secondary dark:text-white">
             5
           </button>
-          <button className="flex h-8 w-8 items-center justify-center text-sm font-medium text-text_secondary">
+          <button className="flex h-8 w-8 items-center justify-center text-sm font-medium text-text_secondary dark:text-white">
             ...
           </button>
           <button className="flex h-8 w-8 items-center justify-center">
-            <FiChevronRight size={20} className="text-secondary_main" />
+            <FiChevronRight
+              size={20}
+              className="text-secondary_main dark:text-white"
+            />
           </button>
         </div>
       </div>
