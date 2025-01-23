ab1cb0794e22784dc00d63045d8b17a5b7719c00
Al Amin <alaminpgmr@gmail.com>
2025-01-23 12:51:11 +0600

    Add profiles data and update ProfileLists component to display dynamic user information

--- src/app/my-account.jsx
+++ src/app/my-account.jsx
@@ -1,11 +1,12 @@
 import ProfileLists from "@/components/screen/Accounts/profile-lists";
 import PageHeading from "@/components/shared/PageHeading";
+import { profiles } from "@/data/profiles.data";
 
 const MyAccountPage = () => {
   return (
     <>
       <PageHeading page_name="My Accounts" />
-      <ProfileLists />
+      <ProfileLists profiles={profiles} />
     </>
   );
 };
