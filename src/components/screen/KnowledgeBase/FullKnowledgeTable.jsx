e8843dbc5dd4cbd783c19554e8b4ed33c4569916
SymuNadeem <msymunnadeem@gmail.com>
2025-01-24 11:30:39 +0600

    sorting icon added

--- src/components/screen/KnowledgeBase/FullKnowledgeTable.jsx
+++ src/components/screen/KnowledgeBase/FullKnowledgeTable.jsx
@@ -10,21 +10,24 @@ import {
 } from "@/components/ui/pagination";
 import { knowledgeTableData } from "@/data/knowledgeTableData";
 import React, { useState } from "react";
+import { FaSortDown, FaSortUp } from "react-icons/fa";
 import writeIcon from "../../../assets/icon/pencil.svg";
-import sortIcon from "../../../assets/icon/sort.svg";
 
 const FullKnowledgeTable = () => {
-  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
+  const [sortConfig, setSortConfig] = useState({
+    key: null,
+    direction: "ascending",
+  });
 
   const sortedData = React.useMemo(() => {
     let sortableItems = [...knowledgeTableData];
     if (sortConfig.key) {
       sortableItems.sort((a, b) => {
         if (a[sortConfig.key] < b[sortConfig.key]) {
-          return sortConfig.direction === 'ascending' ? -1 : 1;
+          return sortConfig.direction === "ascending" ? -1 : 1;
         }
         if (a[sortConfig.key] > b[sortConfig.key]) {
-          return sortConfig.direction === 'ascending' ? 1 : -1;
+          return sortConfig.direction === "ascending" ? 1 : -1;
         }
         return 0;
       });
@@ -33,29 +36,52 @@ const FullKnowledgeTable = () => {
   }, [knowledgeTableData, sortConfig]);
 
   const requestSort = (key) => {
-    let direction = 'ascending';
-    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
-      direction = 'descending';
+    let direction = "ascending";
+    if (sortConfig.key === key && sortConfig.direction === "ascending") {
+      direction = "descending";
     }
     setSortConfig({ key, direction });
   };
 
   return (
     <div className="mt-6 w-80 min-[375px]:w-[300px] min-[425px]:w-[350px] min-[430px]:w-[356px] md:w-[670px] lg:w-[675px] xl:w-full">
-      <div className="relative w-full overflow-x-auto border border-[#0000001A] sm:rounded-lg dark:border-[#FFFFFF1A]">
+      <div className="relative w-full overflow-x-auto border border-[#0000001A] dark:border-[#FFFFFF1A] sm:rounded-lg">
         <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right">
           <thead className="border-b border-[#0000001A] bg-[#4444440D] text-sm font-bold text-[#4A5773] dark:border-[#FFFFFF1A] dark:bg-[#212639] dark:text-[#E4E7EC]">
             <tr>
-              {['title', 'type', 'date', 'source', 'isActive'].map((header, index) => (
-                <th key={index} scope="col" className="p-4">
-                  <div className="flex items-center justify-between">
-                    {header.charAt(0).toUpperCase() + header.slice(1)}
-                    <button onClick={() => requestSort(header)}>
-                      <img src={sortIcon} alt="sortIcon" />
-                    </button>
-                  </div>
-                </th>
-              ))}
+              {["title", "type", "date", "source", "isActive"].map(
+                (header, index) => (
+                  <th key={index} scope="col" className="p-4">
+                    <div className="flex items-center justify-between">
+                      {header.charAt(0).toUpperCase() + header.slice(1)}
+                      <button
+                        onClick={() => requestSort(header)}
+                        className="flex flex-col items-center"
+                      >
+                        <FaSortUp
+                          size={16}
+                          className={`${
+                            sortConfig.key === header &&
+                            sortConfig.direction === "ascending"
+                              ? "text-[#687588]"
+                              : "text-gray-400"
+                          }`}
+                        />
+                        <FaSortDown
+                          size={16}
+                          className={`mt-[-14px] ${
+                            sortConfig.key === header &&
+                            sortConfig.direction === "descending"
+                              ? "text-[#687588]"
+                              : "text-gray-400"
+                          }`}
+                        />
+                      </button>
+                    </div>
+                  </th>
+                ),
+              )}
+
               <th scope="col" className="w-[5%] p-4">
                 Action
               </th>
@@ -67,10 +93,10 @@ const FullKnowledgeTable = () => {
                 key={index}
                 className="border-b bg-white text-sm text-[#101828] dark:border-[#FFFFFF1A] dark:bg-[#161b2f] dark:text-white"
               >
-                <td className="p-4 whitespace-nowrap">{row.title}</td>
-                <td className="p-4 whitespace-nowrap">{row.type}</td>
+                <td className="whitespace-nowrap p-4">{row.title}</td>
+                <td className="whitespace-nowrap p-4">{row.type}</td>
                 <td className="p-4">{row.data}</td>
-                <td className="p-4 whitespace-nowrap">{row.source}</td>
+                <td className="whitespace-nowrap p-4">{row.source}</td>
                 <td className="p-4">
                   <Toggle initialActive={row.isActive} />
                 </td>
