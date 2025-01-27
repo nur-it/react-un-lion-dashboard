01a8ca5d80f336af14222c3351b6eacf19384ba8
Al Amin <alaminpgmr@gmail.com>
2025-01-27 21:31:26 +0600

    Refactor download functionality in Sentiment and Emotions components to directly trigger CSV downloads and remove unused dropdown logic

--- src/components/screen/Home/MentionSentiment/Sentiment.jsx
+++ src/components/screen/Home/MentionSentiment/Sentiment.jsx
@@ -1,5 +1,5 @@
 import TimePeriodDropdown from "@/components/shared/TimePeriodDropdown";
-import { downloadAsCsv, downloadAsJson } from "@/utils/exportUtils";
+import { downloadAsCsv } from "@/utils/exportUtils";
 import { useEffect, useRef, useState } from "react";
 import downloadIcon from "../../../../assets/icon/download.svg";
 import SentimentBarChart from "./SentimentBarChart";
@@ -59,12 +59,12 @@ const Sentiment = () => {
             <div className="relative">
               <button
                 ref={buttonRef}
-                onClick={() => setShowDropdown(!showDropdown)}
+                onClick={() => downloadAsCsv(datasets)}
                 className="flex h-9 items-center justify-center rounded-lg border border-gray300 bg-[#FFFFFF05] px-3 dark:border-[#FFFFFF4D]"
               >
                 <img src={downloadIcon} alt="Download" />
               </button>
-              {showDropdown && (
+              {/*  {showDropdown && (
                 <div
                   ref={dropdownRef}
                   className="absolute right-0 mt-2 w-40 rounded-lg bg-white shadow-lg dark:bg-[#282C3F]"
@@ -82,7 +82,7 @@ const Sentiment = () => {
                     CSV
                   </button>
                 </div>
-              )}
+              )} */}
             </div>
           </div>
         </div>
