def8bc1f0932e6c342470f909273596dd0c8931b
Al Amin <alaminpgmr@gmail.com>
2025-01-27 19:04:35 +0600

    Remove unused authentication methods from avatar service and add listAvatars method

--- src/services/avatar-service.js
+++ src/services/avatar-service.js
@@ -1,16 +1,5 @@
 import requests from "./httpService";
 
 export const authService = {
-  login: async (body) => await requests.post("/login", body),
-  register: async (body) => await requests.post("/register", body),
-
-  resetPassword: async (body) => await requests.post("/reset_password", body),
-
-  forgotPassword: async (body) => await requests.post("/reset_user", body),
-  updateEmail: async (body) => await requests.post("/update_email", body),
-
-  updateFrequency: async (body) =>
-    await requests.post("/update_frequency", body),
-
-  logout: () => requests.get("/logout"),
+  listAvatars: async () => await requests.get("/list_avatars"),
 };
