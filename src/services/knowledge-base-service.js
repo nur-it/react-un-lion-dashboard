7058bf0612d92c9731e533947baf5afbc37986b0
Al Amin <alaminpgmr@gmail.com>
2025-01-27 19:27:30 +0600

    Add knowledge base service with methods for managing knowledge base operations

---
+++ src/services/knowledge-base-service.js
@@ -0,0 +1,18 @@
+import requests from "./httpService";
+
+export const knowledgeBaseService = {
+  knowledgeBase: async () => await requests.get("/knowledge-base"),
+  uploadSourceFile: async (body) =>
+    await requests.post("/upload_source_file", body),
+
+  saveTrustfulSource: async (body) =>
+    await requests.post("/save_trustful_source", body),
+
+  updateToggleKb: async (body) =>
+    await requests.post("/update_toggle_kb", body),
+
+  uploadVideoToCheck: async (body) =>
+    await requests.post("/upload_video_to_check", body),
+  checkProcessingStatus: async () =>
+    await requests.get("/check_processing_status"),
+};
