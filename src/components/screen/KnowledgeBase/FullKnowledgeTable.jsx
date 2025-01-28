10a83a8cadadf05ce3533089c91a2a1037729fca
Al Amin <alaminpgmr@gmail.com>
2025-01-28 11:57:40 +0600

    Refactor sorting logic in FullKnowledgeTable to include reset option for sorting direction

--- src/components/screen/KnowledgeBase/FullKnowledgeTable.jsx
+++ src/components/screen/KnowledgeBase/FullKnowledgeTable.jsx
@@ -48,11 +48,17 @@ const FullKnowledgeTable = () => {
   const requestSort = (key) => {
     setSortConfig((prev) => {
       if (prev.key === key) {
-        return {
-          key,
-          direction:
-            prev.direction === "ascending" ? "descending" : "ascending",
-        };
+        if (prev.direction === "ascending") {
+          return {
+            key,
+            direction: "descending",
+          };
+        } else if (prev.direction === "descending") {
+          return {
+            key: null, // Reset sorting
+            direction: null,
+          };
+        }
       }
       return { key, direction: "ascending" };
     });
